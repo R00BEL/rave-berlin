6726af68465149ceaea7bdca0abe1bc4c7825474
Sasha Karpovich <roobel2005@gmail.com>
2024-07-05 20:00:06 +0300

    wip: Added buy button

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -43,7 +43,7 @@ const HomePage = () => {
         <Testimonials />
       </div>
 
-      <div className="mt-8 flex flex-col gap-2.5 justify-center items-center">
+      <div className="mt-8 flex flex-col gap-2.5 justify-center items-center w-full">
         <div className="px-2.5 py-1 bg-black text-white flex justify-center font-black text-xs w-full">
           SECRET LOCATION // ILLEGAL EXPERIENCE // REAL BERLIN
         </div>
@@ -67,6 +67,28 @@ const HomePage = () => {
 
         <div className="px-2.5 py-1 bg-black text-white flex justify-center font-black text-xs w-full">
           TECHNO // DnB // HOUSE // TRANCE
+        </div>
+      </div>
+
+      <div className="flex justify-center item">
+        <div className="flex flex-col mt-10 bg-white mx-2.5 w-[22.625rem] h-[9rem] rounded-3xl px-5 py-6 justify-between">
+          <div className="flex text-[40px] font-black justify-between">
+            <div>TICKET:</div>
+            <div>20 €</div>
+          </div>
+
+          <div className="flex text-xl font-black justify-between">
+            <div className="flex items-center">PROCEED TO CHECKOUT</div>
+            <div className="flex items-center">
+              <Image
+                src={"/icons/rightArrow.svg"}
+                alt="rightArrow"
+                width={37.5}
+                height={19.5}
+                priority
+              />
+            </div>
+          </div>
         </div>
       </div>
     </main>
